095616b3e29fc88a97d5c687a74b64447e9b6283
luo0412 <1095847440@qq.com>
2018-03-20 18:36:21 +0800

    small

--- ch5-spring-boot/SpringBoot-Learning/Chapter3-1-3/src/main/java/com/didispace/web/HelloController.java
+++ ch5-spring-boot/SpringBoot-Learning/Chapter3-1-3/src/main/java/com/didispace/web/HelloController.java
@@ -3,6 +3,7 @@ package com.didispace.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
