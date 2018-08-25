553e0afe47b928071e62b7891f6e77506297847f
luo0412 <1095847440@qq.com>
2018-08-25 10:40:43 +0800

    [socks]small

--- _book-code/springboot-socks/t_user.sql
+++ _book-code/springboot-socks/t_user.sql
@@ -2,18 +2,19 @@
 -- CREATE DATABASE `socks`;
 -- USE `SOCKS`;
 
+-- 数据库连接测试
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
   `user_id` varchar(50) ,
   `username` varchar(50) ,
-  `password` varchar(50)
+  `password` varchar(50),
+  `phone_num` varchar(15)
 ) ;
 
-INSERT INTO `t_user` VALUES ('1', 'admin', 'admin');
-INSERT INTO `t_user` VALUES ('2', 'yizhiwazi', '123456');
+INSERT INTO `t_user` VALUES ('1', 'admin', 'admin','15011791234');
+INSERT INTO `t_user` VALUES ('2', 'roots', 'roots','18812342017');
 
-
-
+-- 定时任务
 DROP TABLE IF EXISTS `cron`;
 CREATE TABLE `cron`  (
   `cron_id` varchar(30),
