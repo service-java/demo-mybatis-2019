b337369f41464184394c8ee16a5c2d879ee15d28
luo0412 <1095847440@qq.com>
2018-08-28 17:31:27 +0800

    adjust

--- jdk8-boot2-beetl-flowable-guns-admin/_doc/guns.sql
+++ jdk8-boot2-beetl-flowable-guns-admin/_doc/guns.sql
@@ -13,10 +13,10 @@ File Encoding         : 65001
 Date: 2018-06-26 23:10:40
 */
 
-DROP DATABASE IF EXISTS guns;
-CREATE DATABASE IF NOT EXISTS guns DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-
-USE guns;
+-- DROP DATABASE IF EXISTS guns_rest;
+-- CREATE DATABASE IF NOT EXISTS guns DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+--
+-- USE guns_rest;
 
 SET FOREIGN_KEY_CHECKS=0;
 
