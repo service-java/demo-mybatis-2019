c1f3a02877f99405afc32408a159e64352675ce1
luo0412 <1095847440@qq.com>
2018-08-28 21:10:32 +0800

    adjust

--- jdk8-boot2-vue-wx-litemall-mall/litemall-wx/config/api.js
+++ jdk8-boot2-vue-wx-litemall-mall/litemall-wx/config/api.js
@@ -1,8 +1,8 @@
 // 以下是业务服务器API地址
 // 本机开发时使用
- var WxApiRoot = 'http://localhost:8080/wx/';
+//  var WxApiRoot = 'http://localhost:8082/wx/';
 // 局域网测试使用
-// var WxApiRoot = 'http://192.168.0.101:8080/wx/';
+var WxApiRoot = 'http://192.168.1.64:8082/wx/';
 // 云平台部署时使用
 // var WxApiRoot = 'http://122.152.206.172:8080/wx/';
 // 云平台上线时使用
