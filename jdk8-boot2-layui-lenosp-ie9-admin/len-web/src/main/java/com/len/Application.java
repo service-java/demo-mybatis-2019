3324771ce93c02b64ce0a19f01ce1c12a9422d29
luo0412 <1095847440@qq.com>
2018-08-29 14:47:52 +0800

    adjust

--- jdk8-boot2-layui-lenosp-ie9-admin/len-web/src/main/java/com/len/Application.java
+++ jdk8-boot2-layui-lenosp-ie9-admin/len-web/src/main/java/com/len/Application.java
@@ -30,7 +30,7 @@ public class Application {
 //    String[] names = applicationContext.getBeanDefinitionNames();
         //1.8 forEach循环
 //    Arrays.asList(names).forEach(System.out::println);
-        System.out.println("Server start succ");
+        System.out.println("Server start success");
     }
 
 
