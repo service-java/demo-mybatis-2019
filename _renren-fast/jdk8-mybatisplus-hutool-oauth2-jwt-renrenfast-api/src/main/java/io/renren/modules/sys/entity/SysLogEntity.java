0fd5ed9b592010d681436929301873d0f17105f0
luo0412 <1095847440@qq.com>
2019-01-29 11:21:45 +0800

    adjust

---
+++ _renren-fast/jdk8-mybatisplus-hutool-oauth2-jwt-renrenfast-api/src/main/java/io/renren/modules/sys/entity/SysLogEntity.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2018 人人开源 http://www.renren.io
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.renren.modules.sys.entity;
+
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ * 系统日志
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-08 10:40:56
+ */
+@Getter
+@Setter
+@Accessors(chain = true)
+@TableName("sys_log")
+public class SysLogEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@TableId
+	private Long id;
+	//用户名
+	private String username;
+	//用户操作
+	private String operation;
+	//请求方法
+	private String method;
+	//请求参数
+	private String params;
+	//执行时长(毫秒)
+	private Long time;
+	//IP地址
+	private String ip;
+	//创建时间
+	private Date createDate;
+
+}
